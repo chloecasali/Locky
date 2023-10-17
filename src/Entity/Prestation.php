6e5f8f0d9e50f5f1945dd5dd664f72c4a91587df
Chloe Casali <chloecasali2003@gmial.com>
2023-10-17 20:47:25 +0200

    create the prestation and user entities

---
+++ src/Entity/Prestation.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PrestationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PrestationRepository::class)]
+class Prestation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $extrait = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $remuneration = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateDeCreation = null;
+
+    #[ORM\Column]
+    private ?int $numeroDeTelephone = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getExtrait(): ?string
+    {
+        return $this->extrait;
+    }
+
+    public function setExtrait(string $extrait): static
+    {
+        $this->extrait = $extrait;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getRemuneration(): ?float
+    {
+        return $this->remuneration;
+    }
+
+    public function setRemuneration(float $remuneration): static
+    {
+        $this->remuneration = $remuneration;
+
+        return $this;
+    }
+
+    public function getDateDeCreation(): ?\DateTimeInterface
+    {
+        return $this->dateDeCreation;
+    }
+
+    public function setDateDeCreation(\DateTimeInterface $dateDeCreation): static
+    {
+        $this->dateDeCreation = $dateDeCreation;
+
+        return $this;
+    }
+
+    public function getNumeroDeTelephone(): ?int
+    {
+        return $this->numeroDeTelephone;
+    }
+
+    public function setNumeroDeTelephone(int $numeroDeTelephone): static
+    {
+        $this->numeroDeTelephone = $numeroDeTelephone;
+
+        return $this;
+    }
+}
